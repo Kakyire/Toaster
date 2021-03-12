d9bc86fdf07c15cb635fefb21eda46b549394125
Kakyire <47930771+Kakyire@users.noreply.github.com>
2021-03-12 22:44:53 +0000

    Created Fragment extension functions

--- toaster/src/main/java/com/kakyiretechnologies/toaster/Toaster.kt
+++ toaster/src/main/java/com/kakyiretechnologies/toaster/Toaster.kt
@@ -1,22 +1,35 @@
 package com.kakyiretechnologies.toaster
 
+import android.app.Activity
 import android.widget.Toast
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.Fragment
 
 private const val SOMETHING_WENT_WRONG = "Something went wrong. Try again"
 
-fun FragmentActivity.longToast(message: String) {
+fun Activity.longToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
 
-fun FragmentActivity.shortToast(message: String) {
+fun Activity.shortToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-fun FragmentActivity.errorToast() {
+fun Activity.errorToast() {
     Toast.makeText(this, SOMETHING_WENT_WRONG, Toast.LENGTH_SHORT).show()
 }
 
+
+fun Fragment.longToast(message: String) {
+    Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.shortToast(message: String) {
+    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.errorToast() {
+    Toast.makeText(requireContext(), SOMETHING_WENT_WRONG, Toast.LENGTH_SHORT).show()
+}
 
 
 
